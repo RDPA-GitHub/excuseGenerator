9a597911e7cc90ce67fd31d8bcd13df22ef08666
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-02 07:00:40 -0300

    Excuse Generator - Realizacion de Funcion para Generar Excusas Aleatorias

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse_2").innerHTML = excuseGeneraor();
 };
 
 
+let excuseGeneraor = () => {
+  // BSD de Excusas
+    let who = ['The Dog', 'My Granma', 'His Turtle', 'My Bird'];
+    let what = ['eat', 'pissed', 'crushed', 'broked'];
+    let when = ['before the class', 'right in the time', 'when I finished', 'during my lunch', 'while I was playing'];
+
+  // Concatenacion de Excusas
+    let Excusa = `${who[Math.floor(Math.random()*who.length)]} ${what[Math.floor(Math.random()*what.length)]} ${when[Math.floor(Math.random()*when.length)]}`;
+
+  return Excusa;
+} 
